426196e5d70c8a34b5dfa878aed6840a2a0b1392
Ruberval Brasileiro <tecdsis40@gmail.com>
2024-10-04 14:37:16 -0300

    Ajuste conexao ao BD senha e metodos fechamento conn e stmt

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,5 +1,6 @@
 
 import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
@@ -16,13 +17,15 @@ import javax.swing.JOptionPane;
  * @author Adm
  */
 public class conectaDAO {
+    public Connection conn;
+    
     
     public Connection connectDB(){
         Connection conn = null;
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=#senac9865");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
@@ -30,4 +33,15 @@ public class conectaDAO {
         return conn;
     }
     
+    public Connection getConn(){
+        return conn;
+    }
+    
+    public void desconectar(Connection conn, Statement stmt) {
+        try{
+            conn.close();
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível se conectar ao banco de dados");
+        }
+    }
 }
